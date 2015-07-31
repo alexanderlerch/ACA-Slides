bb812e9c003811a13e37fa63becc4f1ffcca5b84
lerch <lerch@zplane.de>
2015-07-31 17:21:19 -0400

    in work version of pitch slides

---
+++ matlab/displayPerceptualPitch.m
@@ -0,0 +1,44 @@
+function displayPerceptualPitch()
+
+    if(exist('ToolFreq2Mel') ~=2)
+        error('Please add the ACA scripts (https://github.com/alexanderlerch/ACA-Code) to your path!');
+    end
+
+    hFigureHandle = generateFigure(10.8,4);
+    
+    [cPath, cName]  = fileparts(mfilename('fullpath'));
+    cOutputFilePath = [cPath '/../graph/' strrep(cName, 'display', '')];
+ 
+    % read sample data
+    [f,fMel,fBark,cLegendMel,cLegendBark]  = generateSampleData();
+
+    subplot(121)
+    semilogx(f/1000,fMel)
+    legend (cLegendMel,'Location','NorthWest');
+    xlabel('$f$ [kHz]')
+    ylabel('Mel')
+    axis([f(1)/1000 f(end)/1000 0 4000])
+
+    subplot(122)
+    semilogx(f/1000,fBark)
+    legend (cLegendBark,'Location','NorthWest');
+    xlabel('$f$ [kHz]')
+    ylabel('Bark')
+    axis([f(1)/1000 f(end)/1000 0 40])
+
+    printFigure(hFigureHandle, cOutputFilePath)
+end
+
+function [f,fMel,fBark,cLegendMel,cLegendBark] = generateSampleData()
+    
+    f = 50:16000;
+    cLegendMel = char('Fant','Shaughnessy', 'Umesh');
+    cLegendBark = char('Schroeder','Terhardt', 'Zwicker');
+
+    for (i = 1:size(cLegendMel,1))
+        fMel(i,:) = ToolFreq2Mel(f,deblank(cLegendMel(i,:)));
+    end
+    for (i = 1:size(cLegendBark,1))
+        fBark(i,:) = ToolFreq2Bark(f,deblank(cLegendBark(i,:)));
+    end
+end
---
+++ matlab/displayPitchHelix.m
@@ -0,0 +1,46 @@
+function displayPerceptualPitch()
+
+    hFigureHandle = generateFigure(10.8,6);
+    
+    [cPath, cName]  = fileparts(mfilename('fullpath'));
+    cOutputFilePath = [cPath '/../graph/' strrep(cName, 'display', '')];
+ 
+    % read sample data
+    [x,y,z]  = generateSampleData();
+
+    plot3(x,y,zeros(size(x)),'Color',[234/256 170/256 0]);
+    hold on;
+    plot3(x,y,z,'k')
+    view(-38,32);
+    set(gca,'XTick',[0 1])
+    set(gca,'YTick',[0 1])
+    zlabel('$f/f_0$');
+    set(gca,'XTickLabel',{''})
+    set(gca,'YTickLabel',{''})
+    set(gca,'ZTick',[1 2 4 8 16])
+    
+    vec = (find( x - 1>-1e-5));
+    plot3(x(vec),y(vec),z(vec),'o','MarkerEdgeColor','k',...
+                'MarkerFaceColor','k',...
+                'MarkerSize',5)
+    
+ 
+    cPitchNames = char('C','C\#','D','D\#','E','F','F\#','G','G\#','A','A\#','B');
+    for (k = 1:12)
+        phase = j*(k-1)*2*pi/12;
+        text(real(.7*exp(phase)),imag(.8*exp(phase)),deblank(cPitchNames(k,:)),'Color',[234/256 170/256 0]);
+    end
+    hold off;
+
+    axis('tight')
+
+    printFigure(hFigureHandle, cOutputFilePath)
+end
+
+function [x,y,z] = generateSampleData()
+    
+    t= linspace(0,8*pi,16384);%0.00:0.01:8*pi;
+    x=cos(t);
+    y=sin(t);
+    z = 2.^(t/(2*pi));
+end
