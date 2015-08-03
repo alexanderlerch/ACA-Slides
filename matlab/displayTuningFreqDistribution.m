f9e27aa4f4263c30b9088a67cda16c3783fa890b
lerch <lerch@zplane.de>
2015-08-03 17:24:06 -0400

    - first rough draft

---
+++ matlab/displayTuningFreqDistribution.m
@@ -0,0 +1,25 @@
+function displayTuningFreqDistribution()
+
+    hFigureHandle = generateFigure(10.8,5);
+    
+    [cPath, cName]  = fileparts(mfilename('fullpath'));
+    cOutputFilePath = [cPath '/../graph/' strrep(cName, 'display', '')];
+ 
+    % read sample data
+    [f,p]  = generateSampleData();
+
+    bar(f,p,'k'), grid on
+    axis([427 453 0 .25])
+
+    xlabel('$f_{A4}$ [Hz]');
+    ylabel('RDF');
+
+    printFigure(hFigureHandle, cOutputFilePath)
+end
+
+function [f,p] = generateSampleData()
+    
+    R       = load('TuningFreqRawData.txt');
+    [p, f]  = hist(R,420:460);
+    p = p/sum(p);
+end
