fde4a7b42b2989bca1a12550c1dea6cdad1bab73
lerch <lerch@zplane.de>
2015-11-16 09:21:28 -0500

    - added warning if for some reason the pdf couldn't be generated

--- matlab/printFigure.m
+++ matlab/printFigure.m
@@ -1,5 +1,7 @@
 function printFigure(hFigureHandle, cOutputFilePath)
-
    print(hFigureHandle, '-depsc', '-tiff', '-r600', '-cmyk', strcat(cOutputFilePath,'.eps'));
    [a,b] = system(sprintf('epstopdf --gsopt=-dPDFSETTINGS=/prepress --outfile=%s.pdf %s.eps',cOutputFilePath,cOutputFilePath));
+   if (~isempty(b))
+       warning('printFigure: eps to pdf conversion failed... ("%s")\n esp2pdf message: "%s"',cOutputFilePath,b);
+   end
 end
